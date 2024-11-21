1b4e0d06996b0246e919b5d1b8abea271a14373c
ringo360 <ringoxd@ringoxd.dev>
2024-11-21 13:15:51 +0900

    clack prompt

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { cancel, intro, isCancel, outro, text } from '@clack/prompts';
+
+intro('Hello');
+const a = await text({
+    message: 'input',
+    placeholder: 'meh',
+});
+if (isCancel(a)) {
+    cancel('Cancelled.');
+    process.exit(0);
+}
+outro('End');
